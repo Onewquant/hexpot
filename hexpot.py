ac8e46dd87cc3d0601054a125dc2c3dd5328f16e
onew <seunghwan0216@gmail.com>
2020-07-27 20:10:47 +0900

    data distribution

--- hexpot.py
+++ hexpot.py
@@ -191,7 +191,7 @@ def generate_day_list(start_date,end_date=None):
         end_date = datetime.strptime(end_date,'%Y-%m-%d')
 
     delta = end_date - start_date
-    date_list = []
+    date_list = list()
 
     for i in range(delta.days + 1):
         d = start_date + timedelta(days=i)
@@ -237,116 +237,74 @@ def mk_new_dir(dir_path):
 ##                          KFinance Data File
 ###########################################################################
 
-def load_single_market_stock_event_data_generator(market,ticker, start_dt_str, end_dt_str, db_path, pb_cls=kstdformat_pb2.kstevent):
+def load_single_ticker_market_event_data_bundle(universe, atype, market, ticker, start_dt_str, end_dt_str, db_path, pb_cls=kfinformat_pb2.kfinevent):
 
-    start_date = start_dt_str.split('T')[0]
-    end_date = end_dt_str.split('T')[0]
+    start_date, end_date = start_dt_str.split('T')[0], end_dt_str.split('T')[0]
 
-    start_ts_str = str(int(datetime.strptime(start_dt_str,'%Y-%m-%dT%H:%M:%S').timestamp()))
-    end_ts_str = str(int(datetime.strptime(end_dt_str,'%Y-%m-%dT%H:%M:%S').timestamp()))
+    to_do_file_path_list = list()
 
-    to_do_candidate_list = generate_day_list(start_date=start_date,end_date=end_date)
+    if atype in ('STOCK','ETF'):
+        file_name = '{}_{}_RM0_{}_*-*-*.gz'.format(atype, market,ticker)
+        file_path = '{}\\{}\\{}\\{}\\{}'.format(db_path, universe, atype, ticker, file_name)
+        file_list = glob.glob(file_path)
 
-    file_name = '{}_{}_****-**-**.gz'.format(market,ticker)
-    file_path = '{}\\{}\\{}\\{}'.format(db_path,market,ticker,file_name)
-    file_list = glob.glob(file_path)
-    to_do_file_path_list = []
-    to_do_date_list = []
+        for fp in file_list:
+            file_date = fp.split('_')[-1][:-3]
+            if (file_date>=start_date)&(file_date<=end_date):
+                to_do_file_path_list.append(fp)
 
-    for fp in file_list:
-        file_date = fp.split('{}_{}_'.format(market,ticker))[-1][:-3]
-        if file_date in to_do_candidate_list:
-            to_do_date_list.append(file_date)
-            to_do_file_path_list.append(fp)
+        to_do_file_path_list.sort(reverse=False)
 
-    to_do_file_path_list.sort(reverse=False)
-
-    for fp_inx in range(len(to_do_file_path_list)):
-        if fp_inx==0:
-            fp = to_do_file_path_list[fp_inx]
-            t = stream.parse(ifp=fp, pb_cls=pb_cls)
-            continue
-        fp = to_do_file_path_list[fp_inx]
-        f = stream.parse(ifp=fp, pb_cls=pb_cls)
-        t = chain(t,f)
-
-    def filtered_gen_func():
-        for x in t:
-            if (x.tms < start_ts_str)|(x.tms >= end_ts_str):
+        for fp_inx, fp_val in enumerate(to_do_file_path_list):
+            if fp_inx == 0:
+                fp = fp_val
+                t = stream.parse(ifp=fp, pb_cls=pb_cls)
                 continue
-            yield x
+            fp = fp_val
+            f = stream.parse(ifp=fp, pb_cls=pb_cls)
+            t = chain(t, f)
 
-    return filtered_gen_func()
-
-
-def load_single_market_stock_event_data_bundle(market, ticker, start_dt_str, end_dt_str, db_path, pb_cls=kstdformat_pb2.kstevent):
-
-    start_date = start_dt_str.split('T')[0]
-    end_date = end_dt_str.split('T')[0]
-
-    start_ts_str = str(int(datetime.strptime(start_dt_str,'%Y-%m-%dT%H:%M:%S').timestamp()))
-    end_ts_str = str(int(datetime.strptime(end_dt_str,'%Y-%m-%dT%H:%M:%S').timestamp()))
-
-    to_do_candidate_list = generate_day_list(start_date=start_date,end_date=end_date)
-
-    file_name = '{}_{}_****-**-**.gz'.format(market,ticker)
-    file_path = '{}\\{}\\{}\\{}'.format(db_path,market,ticker,file_name)
-    file_list = glob.glob(file_path)
-    to_do_file_path_list = []
-    to_do_date_list = []
-
-    for fp in file_list:
-        file_date = fp.split('{}_{}_'.format(market,ticker))[-1][:-3]
-        if file_date in to_do_candidate_list:
-            to_do_date_list.append(file_date)
-            to_do_file_path_list.append(fp)
-
-    to_do_file_path_list.sort(reverse=False)
-
-    for fp_inx in range(len(to_do_file_path_list)):
-        if fp_inx==0:
-            fp = to_do_file_path_list[fp_inx]
-            t = stream.parse(ifp=fp, pb_cls=pb_cls)
-            continue
-        fp = to_do_file_path_list[fp_inx]
-        f = stream.parse(ifp=fp, pb_cls=pb_cls)
-        t = chain(t,f)
-
+    elif atype in ('FUTURES',):
+        for inx, date in enumerate(generate_day_list(start_date=start_date,end_date=end_date)):
+            file_name = '{}_{}_*_{}.gz'.format(atype, ticker, date)
+            fp_val = '{}\\{}\\{}\\{}\\{}\\{}'.format(db_path, universe, atype, ticker.split('_')[0], date, file_name)
+            file_list = glob.glob(fp_val)
+            if len(file_list)!=0:
+                if inx==0:
+                    t = stream.parse(ifp=file_list[0], pb_cls=pb_cls)
+                    continue
+                f = stream.parse(ifp=file_list[0], pb_cls=pb_cls)
+                t = chain(t, f)
     return t
 
 
-def load_combined_stock_event_data_generator(market_ticker_comb, start_dt_str, end_dt_str, db_path, pb_cls=kstdformat_pb2.kstevent):
+def load_combined_tickers_event_data_generator(market_ticker_comb, start_dt_str, end_dt_str, db_path, pb_cls=kfinformat_pb2.kfinevent):
 
     start_ts_str = str(int(datetime.strptime(start_dt_str,'%Y-%m-%dT%H:%M:%S').timestamp()))
     end_ts_str = str(int(datetime.strptime(end_dt_str,'%Y-%m-%dT%H:%M:%S').timestamp()))
 
     market_ticker_bundle_dict = dict()
     bn = 0
-    for m in market_ticker_comb.keys():
-        for c in market_ticker_comb[m]:
-            try:
-                mcfg = load_single_market_stock_event_data_bundle(market=m,ticker=c,start_dt_str=start_dt_str,end_dt_str=end_dt_str,db_path=db_path)
-                market_ticker_bundle_dict.update({bn:mcfg})
-                bn+=1
-            except:
-                print('{} / {} / {} / {} /데이터가 존재하지 않습니다'.format(m,c,start_dt_str,end_dt_str))
-                continue
+    for inx, row in market_ticker_comb.iterrows():
+        try:
+            market_ticker_bundle_dict[bn] = load_single_ticker_market_event_data_bundle(universe=row['universe'], atype=row['type'], market=row['market'], ticker=row['ticker'], start_dt_str=start_dt_str, end_dt_str=end_dt_str, db_path=db_path, pb_cls=pb_cls)
+            bn += 1
+        except:
+            print('{} / {} / {} / {} / {} / {} / 데이터가 존재하지 않습니다'.format(row['universe'], row['type'], row['market'], row['ticker'], start_dt_str, end_dt_str))
+            continue
 
     if len(market_ticker_bundle_dict)==0:
-        print('데이터가 존재하지 않습니다'.format(m, c, start_dt_str, end_dt_str))
+        print('데이터가 존재하지 않습니다')
 
         class finish_msg():
             def __init__(self):
-                self.type = 'finish_flag'
+                self.dtype = 'finish_flag'
         fin = finish_msg()
         return fin
 
-
     def filtered_gen_func():
 
-        comp_dict = dict()
-        comp_tms_dict = dict()
-        glob_rm_bn = set()
+        comp_dict, comp_tms_dict, glob_rm_bn = dict(), dict(), set()
         while True:
 
             ## 최소 시간 이벤트 내보내기
@@ -368,7 +326,7 @@ def load_combined_stock_event_data_generator(market_ticker_comb, start_dt_str, e
                     if len(comp_tms_dict)==0:
                         class finish_msg():
                             def __init__(self):
-                                self.type = 'finish_flag'
+                                self.dtype = 'finish_flag'
                         fin = finish_msg()
                         yield fin
                         return fin
@@ -385,40 +343,6 @@ def load_combined_stock_event_data_generator(market_ticker_comb, start_dt_str, e
                 yield msg
 
     return filtered_gen_func()
-
-
-def get_single_market_stock_event_data_merged_file(market, ticker, start_date, end_date, input_dir_path, output_dir_path, pb_cls=kstdformat_pb2.kstevent):
-
-    to_do_candidate_list = generate_day_list(start_date=start_date,end_date=end_date)
-
-    file_list = glob.glob(input_dir_path+'\\{}_{}_****-**-**.gz'.format(market,ticker))
-    to_do_file_path_list = []
-    to_do_date_list = []
-
-    for fp in file_list:
-        file_date = fp.split('{}_{}_'.format(market,ticker))[-1][:-3]
-        if file_date in to_do_candidate_list:
-            to_do_date_list.append(file_date)
-            to_do_file_path_list.append(fp)
-
-    min_date = min(to_do_date_list)
-    max_date = max(to_do_date_list)
-
-    output_file_name = '{}_{}_{}_{}.gz'.format(market,ticker,min_date.replace('-',''),max_date.replace('-',''))
-    output_file_path = output_dir_path+'\\{}'.format(output_file_name)
-
-    with stream.open(output_file_path,'a') as output_stream_file:
-
-        for cur_date in to_do_date_list:
-            input_file_name = '{}_{}_{}.gz'.format(market, ticker, cur_date)
-            input_file_path = '{}\\{}'.format(input_dir_path,input_file_name)
-
-            input_stream_file = stream.parse(ifp=input_file_path, pb_cls=pb_cls)
-            for x in input_stream_file:
-                output_stream_file.write(x)
-
-    print('Merging Files / Completed / File Name : {}'.format(output_file_name))
-
 
 
 ###########################################################################
@@ -456,7 +380,6 @@ class hexpo_sim_trader():
 class hexpo_sim_mltrainer():
     def __init__(self,rcv_port,spd_port):
         self.rcv_port = rcv_port
-        # self.snd_port = snd_port
         self.spd_port = spd_port
 
         self.port_settings()
@@ -467,9 +390,6 @@ class hexpo_sim_mltrainer():
         self.data_receiver = self.context.socket(zmq.SUB)
         self.data_receiver.connect("tcp://127.0.0.1:{}".format(self.rcv_port))
         self.data_receiver.setsockopt_string(zmq.SUBSCRIBE,"")
-
-        # self.data_sender = self.context.socket(zmq.PUB)
-        # self.data_sender.bind("tcp://127.0.0.1:{}".format(self.snd_port))
 
         self.speed_reporter = self.context.socket(zmq.PUB)
         self.speed_reporter.bind("tcp://127.0.0.1:{}".format(self.spd_port))
@@ -494,7 +414,6 @@ class hexpo_sim_trader_kstock():
     def port_settings(self):
         self.context = zmq.Context()
 
-        # self.data_sender = self.context.socket(zmq.PUB)
         self.data_sender = self.context.socket(zmq.PUSH)
         self.data_sender.connect("tcp://127.0.0.1:{}".format(self.snd_port))
 
